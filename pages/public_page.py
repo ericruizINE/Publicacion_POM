c188b5c724ea54b11e24626b621af4fff41e2522
ericruizINE <eric.ruiz@ine.mx>
2024-10-22 12:07:49 -0600

    update utils

--- pages/public_page.py
+++ pages/public_page.py
@@ -31,30 +31,3 @@ class PublicPage(BasePage):
         self.capture_element_screenshot(element, file_path)
         print(f"Captura de pantalla guardada en: {file_path}")
         return file_path  # Retorna la ruta de la captura de pantalla
-        
-
-    # Función para leer datos desde el CSV y eliminar el BOM si está presente
-    def leer_datos_csv(filepath):
-        df = pd.read_csv(filepath, encoding='utf-8-sig')
-
-        for index, row in df.iterrows():
-            yield row['allure_story'], row['valor'], row['tipo_dato'], row['selector'], row['ruta']
-    
-    def df():
-        # Leer el archivo CSV en un DataFrame
-        csv_path = '/var/jenkins_home/workspace/Publicacion_POM/data/PRES_2024.csv'
-        df = pd.read_csv(csv_path, skiprows=3, nrows=1, header=None, names=[
-            "ACTAS_ESPERADAS", "ACTAS_REGISTRADAS", "ACTAS_FUERA_CATALOGO", 
-            "ACTAS_CAPTURADAS", "PORCENTAJE_ACTAS_CAPTURADAS", 
-            "ACTAS_CONTABILIZADAS", "PORCENTAJE_ACTAS_CONTABILIZADAS", 
-            "PORCENTAJE_ACTAS_INCONSISTENCIAS", "ACTAS_NO_CONTABILIZADAS", 
-            "LISTA_NOMINAL_ACTAS_CONTABILIZADAS", "TOTAL_VOTOS_C_CS", 
-            "TOTAL_VOTOS_S_CS", "PORCENTAJE_PARTICIPACION_CIUDADANA"
-        ])
-
-        # Retornar solo las columnas necesarias en un nuevo DataFrame
-        selected_columns = df[[
-            "ACTAS_ESPERADAS", "ACTAS_CAPTURADAS", "ACTAS_CONTABILIZADAS", "LISTA_NOMINAL_ACTAS_CONTABILIZADAS", "TOTAL_VOTOS_C_CS", "TOTAL_VOTOS_S_CS", "PORCENTAJE_ACTAS_CAPTURADAS", "PORCENTAJE_PARTICIPACION_CIUDADANA"
-        ]]
-
-        return selected_columns
--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,65 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import allure
+import os
 
 class Utils:
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.screenshots_folder = 'screenshots_publi'
+        self.ensure_screenshot_folder()
+
+    def ensure_screenshot_folder(self):
+        if not os.path.exists(self.screenshots_folder):
+            os.makedirs(self.screenshots_folder)
+
+    def get_next_screenshot_path(self, screenshots_folder, base_filename):
+        """Genera el siguiente nombre de archivo disponible con un número consecutivo."""
+        i = 1
+        while True:
+            filename = f"{base_filename}_{i:03d}.png"
+            path = os.path.join(screenshots_folder, filename)
+            if not os.path.exists(path):
+                return path
+            i += 1
+
+    def highlight_and_capture_element(self, elemento, screenshots_folder):
+        """Resalta y captura un elemento específico de la página pública."""
+        element = elemento
+        file_path = self.get_next_screenshot_path(screenshots_folder, 'captura_elemento')
+        self.capture_element_screenshot(element, file_path)
+        print(f"Captura de pantalla guardada en: {file_path}")
+        return file_path  # Retorna la ruta de la captura de pantalla
+    
+    def capture_element_screenshot(self, element, file_path):
+        """
+        Captura una captura de pantalla resaltando el elemento específico con un borde.
+        """
+        # Obtener el tamaño total de la página
+        total_width = self.driver.execute_script("return document.body.scrollWidth")
+        total_height = self.driver.execute_script("return document.body.scrollHeight")
+
+        # Establecer el tamaño de la ventana al tamaño total de la página
+        self.driver.set_window_size(total_width, total_height)
+        # Resaltar el elemento usando JavaScript
+        self.driver.execute_script("arguments[0].style.border='9px solid red'", element)
+        
+        # Desplazar la página hasta que el elemento esté visible
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)
+
+        # Esperar a que el elemento sea visible
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of(element)
+        )
+
+        # Capturar la pantalla completa
+        self.driver.save_screenshot(file_path)
+
+        #allure.attach(self.driver.get_screenshot_as_png(), name="Element Screenshot", attachment_type=AttachmentType.PNG)
+
+        # Quitar el borde después de la captura
+        self.driver.execute_script("arguments[0].style.border=''", element)
+
     @staticmethod
     def wait_for_element(driver, locator, timeout=10):
         WebDriverWait(driver, timeout).until(EC.presence_of_element_located(locator))
