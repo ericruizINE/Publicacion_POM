9d44e635f80de5291cc49978338f1924286d867a
ericruizINE <eric.ruiz@ine.mx>
2024-10-23 17:30:23 -0600

    update files

--- tests/test_csv_presidencia.py
+++ tests/test_csv_presidencia.py
@@ -4,7 +4,7 @@ import re
 import pytest
 import allure
 
-file_path = '/var/jenkins_home/workspace/Publicacion/Archivos/PRES_2024.csv'
+file_path = '/var/jenkins_home/workspace/Publicacion_POM/tests/data/PRES_2024.csv'
 df = pd.read_csv(file_path, skiprows=4, delimiter=',', low_memory=False)  # Cambia ';' por el delimitador correcto
 df1 = pd.read_csv(file_path, skiprows=3, nrows=1, header=None, names=["ACTAS_ESPERADAS","ACTAS_REGISTRADAS","ACTAS_FUERA_CATALOGO","ACTAS_CAPTURADAS","PORCENTAJE_ACTAS_CAPTURADAS","ACTAS_CONTABILIZADAS","PORCENTAJE_ACTAS_CONTABILIZADAS","PORCENTAJE_ACTAS_INCONSISTENCIAS","ACTAS_NO_CONTABILIZADAS","LISTA_NOMINAL_ACTAS_CONTABILIZADAS","TOTAL_VOTOS_C_CS","TOTAL_VOTOS_S_CS","PORCENTAJE_PARTICIPACION_CIUDADANA"])
 
